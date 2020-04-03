b63dbfbadece9f3e21eb07761d729f942e75fe30
Marcus <davi2812@dee.ufc.br>
2020-04-03 16:02:15 -0300

    MLP implementada

---
+++ Coursera/ex3/lrCostFunction.m
@@ -0,0 +1,61 @@
+function [J, grad] = lrCostFunction(theta, X, y, lambda)
+%LRCOSTFUNCTION Compute cost and gradient for logistic regression with 
+%regularization
+%   J = LRCOSTFUNCTION(theta, X, y, lambda) computes the cost of using
+%   theta as the parameter for regularized logistic regression and the
+%   gradient of the cost w.r.t. to the parameters. 
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly 
+J = 0;
+grad = zeros(size(theta));
+features = size(X,2);
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost of a particular choice of theta.
+%               You should set J to the cost.
+%               Compute the partial derivatives and set grad to the partial
+%               derivatives of the cost w.r.t. each parameter in theta
+%
+% Hint: The computation of the cost function and gradients can be
+%       efficiently vectorized. For example, consider the computation
+%
+%           sigmoid(X * theta)
+%
+%       Each row of the resulting matrix will contain the value of the
+%       prediction for that example. You can make use of this to vectorize
+%       the cost function and gradient computations. 
+%
+% Hint: When computing the gradient of the regularized cost function, 
+%       there're many possible vectorized solutions, but one solution
+%       looks like:
+%           grad = (unregularized gradient for logistic regression)
+%           temp = theta; 
+%           temp(1) = 0;   % because we don't add anything for j = 0  
+%           grad = grad + YOUR_CODE_HERE (using the temp variable)
+%
+
+z = X*theta;
+
+h = sigmoid(z);
+J = -(y'*log(h) + (1-y')*log(1-h))/m + lambda*(theta(2:end)'*theta(2:end))/(2*m); %Vectorized. Ignore theta_0
+
+    e = (h - y);
+    grad(1,1) = sum(e)/m; %ones
+    for feat=2:features
+    grad(feat,1) = sum(e.*X(:,feat))/m + lambda*theta(feat)/m;
+    end
+
+
+
+
+
+
+
+
+% =============================================================
+
+grad = grad(:);
+
+end
